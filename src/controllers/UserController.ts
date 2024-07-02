a28948540660a30294655b18c2e2fbf8ee074db0
andyk2000 <125449101+andyk2000@users.noreply.github.com>
2024-07-02 14:41:32 +0200

    feat(email): changed the email format on sign-up

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -10,17 +10,25 @@ import { Request, Response } from "express";
 import Crypto from "crypto";
 import nodemailer from "nodemailer";
 import jwt from "jsonwebtoken";
+import fs from "fs";
+import { promisify } from "util";
+import ejs from "ejs";
+import path from "path";
+
+const readFileAsync = promisify(fs.readFile);
 
 interface Config {
   email: string;
   password: string;
   secretKey: string;
+  frontendLink: string;
 }
 
 const config: Config = {
   email: process.env.EMAIL_ADDRESS || "andyirimbere@gmail.com",
   password: process.env.EMAIL_PASSWORD || "cykvvsvmijvpqxwr",
   secretKey: process.env.SECRET_KEY || "zero",
+  frontendLink: process.env.FRONTEND_LINK || "http://localhost:3000",
 };
 
 const generateAccessToken = (email: string, id: number) => {
@@ -36,42 +44,74 @@ const generateAccessToken = (email: string, id: number) => {
   );
 };
 
-const confirmationEmail = async (email: string) => {
-  const sender = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: config.email,
-      pass: config.password,
-    },
-    debug: true,
-    logger: true,
-  });
+const confirmationEmail = async (userEmail: string, names: string) => {
+  try {
+    const imageAttachment = await readFileAsync(
+      path.join(__dirname, "../views/images/shopping-bag.png"),
+      { encoding: "base64" },
+    );
 
-  const newMail = {
-    from: config.email,
-    to: email,
-    subject: "welcome to our online shop",
-    text: "Welcome to Our Online Shopping app, the best platform to connect you with your customers and stores across Rwanda",
-  };
+    const emailSent = await ejs.renderFile(
+      path.join(__dirname, "../views/welcomeEmail.ejs"),
+      {
+        email: userEmail,
+        user_firstname: names,
+        confirm_link: config.frontendLink,
+      },
+    );
 
-  sender.sendMail(newMail, (error, info) => {
-    if (error) {
-      console.log("Error sending email:", error);
-      throw error;
-    } else {
-      console.log("Email sent:", info.response);
-    }
-  });
-  console.log(config.email, config.password);
+    const sender = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: config.email,
+        pass: config.password,
+      },
+      debug: true,
+      logger: true,
+    });
+
+    const newMail = {
+      from: config.email,
+      to: userEmail,
+      subject: "Welcome to our online shop",
+      html: emailSent,
+      attachments: [
+        {
+          filename: "shopping-bag.png",
+          content: imageAttachment,
+          encoding: "base64",
+          cid: "logo",
+        },
+      ],
+    };
+
+    sender.sendMail(newMail, (error, info) => {
+      if (error) {
+        console.log("Error sending email:", error);
+        throw error;
+      } else {
+        console.log("Email sent:", info.response);
+      }
+    });
+  } catch (error) {
+    console.error("Error in sending confirmation email:", error);
+    throw error; // Ensure errors are propagated
+  }
 };
 
 const createNewUser = async (request: Request, response: Response) => {
   const { names, email, password, role } = request.body;
   try {
-    const data = await createUser({ names, email, password, role });
-    return response.status(201).json(data);
+    const hashedPassword = encryptPassword(password);
+    const newUser = await createUser({
+      names,
+      email,
+      password: hashedPassword,
+      role,
+    });
+    return response.status(201).json(newUser);
   } catch (error) {
-    console.error(error);
+    console.error("Error creating new user:", error);
     return response.status(500).json({ error: "Internal Server Error" });
   }
 };
@@ -81,8 +121,8 @@ const getAllUsers = async (request: Request, response: Response) => {
     const data = await getUsers();
     return response.status(200).json(data);
   } catch (error) {
-    console.log(error);
-    return response.status(500).json({ error: "failed to get the data" });
+    console.error("Error getting all users:", error);
+    return response.status(500).json({ error: "Failed to get the data" });
   }
 };
 
@@ -92,8 +132,10 @@ const getUserByID = async (request: Request, response: Response) => {
     const userData = await getUserID(id);
     return response.status(200).json(userData);
   } catch (err) {
-    console.log(err);
-    response.status(500).json({ error: `failed to get user with id${id}` });
+    console.error("Error getting user by ID:", err);
+    return response
+      .status(500)
+      .json({ error: `Failed to get user with id ${id}` });
   }
 };
 
@@ -103,8 +145,10 @@ const deleteUserData = async (request: Request, response: Response) => {
     const deletedUser = await deleteUser(id);
     return response.status(200).json(deletedUser);
   } catch (error) {
-    console.log(error);
-    response.status(500).json({ error: `failed to delete user with id${id}` });
+    console.error("Error deleting user:", error);
+    return response
+      .status(500)
+      .json({ error: `Failed to delete user with id ${id}` });
   }
 };
 
@@ -115,10 +159,10 @@ const updateUserData = async (request: Request, response: Response) => {
     const updatedUser = await updateUser({ names, email, password, role }, id);
     return response.status(200).json(updatedUser);
   } catch (error) {
-    console.log(error);
-    response
+    console.error("Error updating user:", error);
+    return response
       .status(500)
-      .json({ error: `failed to update the user with id${id}` });
+      .json({ error: `Failed to update user with id ${id}` });
   }
 };
 
@@ -134,13 +178,13 @@ const signUp = async (request: Request, response: Response) => {
   password = encryptPassword(password);
   try {
     const newUser = await createUser({ names, email, password, role });
-    confirmationEmail(email);
+    confirmationEmail(email, names);
     return response.status(200).json(newUser);
   } catch (error) {
-    console.log(error);
+    console.error("Error signing up user:", error);
     return response
       .status(500)
-      .json({ error: "could not create the new user" });
+      .json({ error: "Could not create the new user" });
   }
 };
 
@@ -149,18 +193,16 @@ const logIn = async (request: Request, response: Response) => {
   const encrypted = encryptPassword(password);
   try {
     const user = await getUserEmail(email);
-    if (user === null) {
-      return response.status(404).json({ error: "login failed try again" });
-    }
-    if (user.password !== encrypted) {
-      return response.status(404).json({ error: "login failed try again" });
+    if (!user || user.password !== encrypted) {
+      return response
+        .status(404)
+        .json({ error: "Login failed. Please try again." });
     }
     const token = generateAccessToken(user.email, user.id);
-
-    return response.status(200).json({ token: token });
+    return response.status(200).json({ token });
   } catch (error) {
-    console.log(error);
-    return response.status(500).json({ error: "failed to login" });
+    console.error("Error logging in user:", error);
+    return response.status(500).json({ error: "Failed to login" });
   }
 };
 
